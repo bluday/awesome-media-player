248fd61c1a9dcbf4abf74a01f756c8ad6e1f504f
BluDay <19403893+BluDay@users.noreply.github.com>
2024-07-19 15:57:00 +0200

    feat: add new MediaControlBar user control

---
+++ src/BluDay.AwesomeMediaPlayer/Controls/MediaControlBar.xaml.cs
@@ -0,0 +1,12 @@
+namespace BluDay.AwesomeMediaPlayer.Controls;
+
+/// <summary>
+/// Represents the media control bar visible on the main view.
+/// </summary>
+public sealed partial class MediaControlBar : UserControl
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MediaControlBar"/> class.
+    /// </summary>
+    public MediaControlBar() => InitializeComponent();
+}
