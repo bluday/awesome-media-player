5802f964420b8d083b3c5dd1c05d2af37bab3072
BluDay <19403893+BluDay@users.noreply.github.com>
2024-07-20 00:46:11 +0200

    refactor: use separate partial class for dependency properties and add docs

--- src/BluDay.AwesomeMediaPlayer/Controls/TitleBar.xaml.cs
+++ src/BluDay.AwesomeMediaPlayer/Controls/TitleBar.xaml.cs
@@ -1,7 +1,39 @@
 namespace BluDay.AwesomeMediaPlayer.Controls;
 
+/// <summary>
+/// Represents the title bar control.
+/// </summary>
 public sealed partial class TitleBar : UserControl
 {
+    /// <summary>
+    /// Gets the image source for the icon.
+    /// </summary>
+    public ImageSource? Icon
+    {
+        get => GetValue(IconProperty) as ImageSource;
+        set => SetValue(IconProperty, value);
+    }
+
+    /// <summary>
+    /// Gets the title value of the title bar.
+    /// </summary>
+    public string? Title
+    {
+        get => GetValue(TitleProperty) as string;
+        set => SetValue(TitleProperty, value);
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="TitleBar"/> class.
+    /// </summary>
+    public TitleBar() => InitializeComponent();
+}
+
+public sealed partial class TitleBar
+{
+    /// <summary>
+    /// Identifies the Icon dependency property.
+    /// </summary>
     public static readonly DependencyProperty IconProperty = DependencyProperty.Register(
         nameof(Icon),
         typeof(ImageSource),
@@ -9,24 +41,13 @@ public sealed partial class TitleBar : UserControl
         new PropertyMetadata(defaultValue: null)
     );
 
+    /// <summary>
+    /// Identifies the Title dependency property.
+    /// </summary>
     public static readonly DependencyProperty TitleProperty = DependencyProperty.Register(
         nameof(Title),
         typeof(string),
         typeof(TitleBar),
         new PropertyMetadata(defaultValue: null)
     );
-
-    public ImageSource? Icon
-    {
-        get => GetValue(IconProperty) as ImageSource;
-        set => SetValue(IconProperty, value);
-    }
-
-    public string? Title
-    {
-        get => GetValue(TitleProperty) as string;
-        set => SetValue(TitleProperty, value);
-    }
-
-    public TitleBar() => InitializeComponent();
 }
