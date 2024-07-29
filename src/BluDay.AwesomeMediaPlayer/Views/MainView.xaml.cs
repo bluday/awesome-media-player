ad4b97f4a82cc3ee0a4f3e81587cbcbfa58eabd3
BluDay <19403893+BluDay@users.noreply.github.com>
2024-07-29 12:18:54 +0200

    feat: add new MainView control and Views namespace

---
+++ src/BluDay.AwesomeMediaPlayer/Views/MainView.xaml.cs
@@ -0,0 +1,12 @@
+namespace BluDay.AwesomeMediaPlayer.Views;
+
+/// <summary>
+/// Interaction logic for MainView.xaml.
+/// </summary>
+public sealed partial class MainView : UserControl
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MainView"/> class.
+    /// </summary>
+    public MainView() => InitializeComponent();
+}
