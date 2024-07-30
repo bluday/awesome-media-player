b73f37b64822179b2447d3ef469533a9250b905f
BluDay <19403893+BluDay@users.noreply.github.com>
2024-07-30 22:19:06 +0200

    feat: add new entrypoint file

--- src/BluDay.AwesomeMediaPlayer/Usings.cs
+++ src/BluDay.AwesomeMediaPlayer/Usings.cs
@@ -2,6 +2,7 @@
 global using CommunityToolkit.Mvvm.Messaging;
 global using CommunityToolkit.WinUI.UI.Controls;
 
+global using BluDay.AwesomeMediaPlayer;
 global using BluDay.AwesomeMediaPlayer.Abstractions.ViewModels;
 global using BluDay.AwesomeMediaPlayer.ComponentModel;
 global using BluDay.AwesomeMediaPlayer.Controls;
@@ -10,6 +11,7 @@ global using BluDay.AwesomeMediaPlayer.ViewModels;
 global using BluDay.AwesomeMediaPlayer.Views;
 
 global using Microsoft.UI;
+global using Microsoft.UI.Dispatching;
 global using Microsoft.UI.Windowing;
 global using Microsoft.UI.Xaml;
 global using Microsoft.UI.Xaml.Controls;
@@ -18,5 +20,7 @@ global using Microsoft.Windows.ApplicationModel.Resources;
 
 global using System;
 global using System.Drawing;
+global using System.Runtime.InteropServices;
+global using System.Threading;
 
 global using Windows.Graphics;
