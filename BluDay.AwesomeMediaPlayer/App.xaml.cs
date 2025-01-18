12a5014cede5688b955be6011d8b6fff9ad3563a
BluDay <19403893+bluday@users.noreply.github.com>
2025-01-18 12:14:18 +0100

    refactor: remove experimental `App.config.cs` file

--- BluDay.AwesomeMediaPlayer/App.xaml.cs
+++ BluDay.AwesomeMediaPlayer/App.xaml.cs
@@ -37,4 +37,54 @@ public sealed partial class App : Application
     {
         _shell.Value.Activate();
     }
+
+    /// <summary>
+    /// Configures the logging factory and provider through the provided builder instance.
+    /// </summary>
+    /// <param name="logging">
+    /// The logging builder instance.
+    /// </param>
+    public static void ConfigureLogging(ILoggingBuilder logging)
+    {
+        logging
+            .AddConsole()
+            .AddDebug()
+            .SetMinimumLevel(LogLevel.Debug);
+    }
+
+    /// <summary>
+    /// Configures and registers platform-specific service descriptors.
+    /// </summary>
+    /// <param name="services">
+    /// The service descriptor collection.
+    /// </param>
+    public static void ConfigureServices(IServiceCollection services)
+    {
+        services
+            .AddLogging(ConfigureLogging);
+
+        services
+            .AddSingleton<AppActivationService>()
+            .AddSingleton<AppDialogService>()
+            .AddSingleton<AppNavigationService>()
+            .AddSingleton<AppThemeService>()
+            .AddSingleton<AppWindowService>();
+
+        services
+            .AddSingleton<ImplementationProvider<IBluWindow>>();
+
+        services
+            .AddSingleton(WeakReferenceMessenger.Default);
+
+        services
+            .AddSingleton<App>()
+            .AddSingleton<ResourceLoader>();
+
+        services
+            .AddTransient<Shell>();
+
+        services
+            .AddViews()
+            .AddViewModels();
+    }
 }
