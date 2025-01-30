aaee9e9667cafd1f6d95738b14941b0b0bea7069
BluDay <19403893+bluday@users.noreply.github.com>
2025-01-31 00:11:01 +0100

    docs: switch lines in the entrypoint header

--- BluDay.AwesomeMediaPlayer/Program.cs
+++ BluDay.AwesomeMediaPlayer/Program.cs
@@ -3,9 +3,9 @@
  * 
  * https://github.com/bluday/awesome-media-player-winui3
  * 
- * MIT License
- * 
  * Copyright (c) 2025 BluDay
+ * 
+ * MIT License
  * 
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
