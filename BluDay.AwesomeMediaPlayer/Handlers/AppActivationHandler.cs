68de4f12f3e23cd15fd52b5a37c2ea261e506902
BluDay <19403893+bluday@users.noreply.github.com>
2025-03-13 15:04:59 +0100

    feat: add new app activation and deactivation classes

---
+++ BluDay.AwesomeMediaPlayer/Handlers/AppActivationHandler.cs
@@ -0,0 +1,30 @@
+﻿namespace BluDay.AwesomeMediaPlayer.Handlers;
+
+/// <summary>
+/// Handles the specific activation procedures for the application.
+/// </summary>
+public sealed class AppActivationHandler : IAppActivationHandler
+{
+    private readonly IAppWindowService _windowService;
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="AppActivationHandler"/> class.
+    /// </summary>
+    /// <param name="windowService">
+    /// The service used to create and manage windows within the running app.
+    /// </param>
+    public AppActivationHandler(IAppWindowService windowService)
+    {
+        _windowService = windowService;
+    }
+
+    /// <inheritdoc cref="IAppActivationHandler.ActivateAsync(object?)"/>
+    public Task ActivateAsync(object? args)
+    {
+        _windowService
+            .CreateWindow<Shell>()
+            .Activate();
+
+        return Task.CompletedTask;
+    }
+}
