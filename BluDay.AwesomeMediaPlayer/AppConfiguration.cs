cd23d95df82a758d729c7134fc81b39693de6d00
BluDay <19403893+bluday@users.noreply.github.com>
2025-03-13 15:04:46 +0100

    refactor: move DI container configuration methods to new class

---
+++ BluDay.AwesomeMediaPlayer/AppConfiguration.cs
@@ -0,0 +1,52 @@
+﻿namespace BluDay.AwesomeMediaPlayer;
+
+/// <summary>
+/// Contains methods for configuring logging and services in the application.
+/// </summary>
+public static class AppConfiguration
+{
+    /// <summary>
+    /// Configures the logging factory and provider through the provided builder instance.
+    /// </summary>
+    /// <param name="logging">
+    /// The logging builder instance.
+    /// </param>
+    public static void ConfigureLogging(ILoggingBuilder logging)
+    {
+        logging
+            .AddConsole()
+            .AddDebug();
+
+        logging
+            .SetMinimumLevel(LogLevel.Debug);
+    }
+
+    /// <summary>
+    /// Configures and registers platform-specific service descriptors.
+    /// </summary>
+    /// <param name="services">
+    /// The service descriptor collection.
+    /// </param>
+    public static void ConfigureServices(IServiceCollection services)
+    {
+        services
+            .AddLogging(ConfigureLogging);
+
+        services
+            .AddSingleton(WeakReferenceMessenger.Default);
+
+        services
+            .AddSingleton<App>()
+            .AddSingleton<ResourceLoader>();
+
+        services
+            .AddTransient<Shell>();
+
+        services
+            .AddDesktopClientServices();
+
+        services
+            .AddViews()
+            .AddViewModels();
+    }
+}
