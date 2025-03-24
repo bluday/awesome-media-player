8709f1d69fe6c00bf0efd855365a2be6f74b563f
BluDay <19403893+bluday@users.noreply.github.com>
2025-03-24 23:16:04 +0100

    feat: hide settings navigation view item

--- BluDay.AwesomeMediaPlayer/Views/MainView.xaml.cs
+++ BluDay.AwesomeMediaPlayer/Views/MainView.xaml.cs
@@ -1,29 +1,20 @@
 namespace BluDay.AwesomeMediaPlayer.Views;
 
 /// <summary>
-/// Interaction logic for MainView.xaml
+/// Represents the interaction logic for the main view.
 /// </summary>
 public sealed partial class MainView : UserControl
 {
-    private readonly Lazy<SettingsView> _settingsView;
-
     /// <summary>
     /// Initializes a new instance of the <see cref="MainView"/> class.
     /// </summary>
     /// <param name="viewModel">
     /// A transient <see cref="MainViewModel"/> instance.
     /// </param>
-    public MainView(MainViewModel viewModel, IServiceProvider serviceProvider)
+    public MainView(MainViewModel viewModel)
     {
-        _settingsView = new Lazy<SettingsView>(serviceProvider.GetRequiredService<SettingsView>);
-
         DataContext = viewModel;
 
         InitializeComponent();
-    }
-
-    private void NavigationView_ItemInvoked(NavigationView sender, NavigationViewItemInvokedEventArgs args)
-    {
-        ViewContentControl.Content = args.IsSettingsInvoked ? _settingsView.Value : null;
     }
 }
