f06098fd8f886b04c4ea66de0c46dcc78e37ac3b
BluDay <19403893+bluday@users.noreply.github.com>
2025-03-28 13:38:13 +0100

    refactor: create a new control for the menu bar

---
+++ BluDay.AwesomeMediaPlayer/Controls/MenuBar.xaml.cs
@@ -0,0 +1,12 @@
+namespace BluDay.AwesomeMediaPlayer.Controls;
+
+/// <summary>
+/// Represents the top menu bar of the application.
+/// </summary>
+public sealed partial class MenuBar : UserControl
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MenuBar"/> class.
+    /// </summary>
+    public MenuBar() => InitializeComponent();
+}
