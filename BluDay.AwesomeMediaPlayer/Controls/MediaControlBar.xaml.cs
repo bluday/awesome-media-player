bef0c80cae3f477a515424b0abcc331aba402b45
BluDay <19403893+bluday@users.noreply.github.com>
2025-04-10 12:31:12 +0200

    feat: add dependency property for corner drag handle in `MediaControlbar`

--- BluDay.AwesomeMediaPlayer/Controls/MediaControlBar.xaml.cs
+++ BluDay.AwesomeMediaPlayer/Controls/MediaControlBar.xaml.cs
@@ -5,6 +5,23 @@ namespace BluDay.AwesomeMediaPlayer.Controls;
 /// </summary>
 public sealed partial class MediaControlBar : UserControl
 {
+    #region Dependency properties
+    public static readonly DependencyProperty CornerDragHandleVisibilityProperty = DependencyProperty.Register(
+        nameof(CornerDragHandleVisibility),
+        typeof(Visibility),
+        typeof(MenuBar),
+        new PropertyMetadata(defaultValue: Visibility.Visible)
+    );
+    #endregion
+
+    #region Properties
+    public Visibility CornerDragHandleVisibility
+    {
+        get => (Visibility)GetValue(CornerDragHandleVisibilityProperty);
+        set => SetValue(CornerDragHandleVisibilityProperty, value);
+    }
+    #endregion
+
     /// <summary>
     /// Initializes a new instance of the <see cref="MediaControlBar"/> class.
     /// </summary>
