003d4fe687eb576459f5f7540def634bda001c45
BluDay <19403893+bluday@users.noreply.github.com>
2025-04-13 14:35:50 +0200

    refactor: rename `Shell` to `MainWindow`

---
+++ BluDay.AwesomeMediaPlayer/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,12 @@
+﻿namespace BluDay.AwesomeMediaPlayer.ViewModels;
+
+/// <summary>
+/// Represents the view model for the app shell.
+/// </summary>
+public sealed partial class MainWindowViewModel
+{
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MainWindowViewModel"/> class.
+    /// </summary>
+    public MainWindowViewModel() { }
+}
