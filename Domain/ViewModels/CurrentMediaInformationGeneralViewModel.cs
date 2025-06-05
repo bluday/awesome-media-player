c8b558d621dedefdd41e30268428804c7335fc70
BluDay <19403893+bluday@users.noreply.github.com>
2025-06-05 21:10:59 +0200

    refactor: rename view

---
+++ Domain/ViewModels/CurrentMediaInformationGeneralViewModel.cs
@@ -0,0 +1,15 @@
+﻿namespace BluDay.AwesomeMediaPlayer.Domain.ViewModels;
+
+/// <summary>
+/// Represents the view model class for the current media information view.
+/// </summary>
+public sealed partial class CurrentMediaInformationGeneralViewModel : ObservableObject
+{
+    /// <summary>
+    /// Initialize a new instance of the <see cref="CurrentMediaInformationGeneralViewModel"/> class.
+    /// </summary>
+    public CurrentMediaInformationGeneralViewModel()
+    {
+        // TODO: Set fields and properties.
+    }
+}
