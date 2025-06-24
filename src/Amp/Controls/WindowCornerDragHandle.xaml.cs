eacef40f1ccf98e03f5c374fdbfd27005f8fef4f
BluDay <19403893+bluday@users.noreply.github.com>
2025-06-24 14:18:55 +0200

    refactor: create separate control for the window corner drag handle

---
+++ src/Amp/Controls/WindowCornerDragHandle.xaml.cs
@@ -0,0 +1,15 @@
+namespace Amp.Controls;
+
+/// <summary>
+/// Interaction logic for WindowCornerDragHandle.xaml.
+/// </summary>
+public sealed partial class WindowCornerDragHandle : Microsoft.UI.Xaml.Controls.UserControl
+{
+    /// <summary>
+    /// Initalizes a new instance of the <see cref="WindowCornerDragHandle"/> class.
+    /// </summary>
+    public WindowCornerDragHandle()
+    {
+        InitializeComponent();
+    }
+}
